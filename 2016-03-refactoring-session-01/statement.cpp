8c45b6dee3824da26c542c138d26995407da35f3
Arne Mertz <void@arne-mertz.de>
2016-03-31 15:47:07 +0200

    Single line blocks
    Blocks that consist of a single line, e.g. of function bodies and for
    loops, should be wrapped in their own curly braces and put on their own
    line. Putting things on their own line visibly separates the two
    separate parts of the loop - the header and the loop body. Adding the
    braces even on one-liners prevent errors that arise from adding more
    lines to the apparent block without adding the braces then.
    
    This is somewhat a matter of taste and coding style, but many style
    guides stick at least to the own line for loop bodies. Most people seem
    to favor the separation over terseness.

--- 2016-03-refactoring-session-01/statement.cpp
+++ 2016-03-refactoring-session-01/statement.cpp
@@ -12,11 +12,17 @@ struct List {
   List& operator=(List const&) = default;
   List& operator=(List&&) = default;
  
-  inline void Append( T const * t ) { _list.push_back( t ); }
+  inline void Append( T const * t ) {
+    _list.push_back( t );
+  }
   typedef typename std::vector<T const *>::const_iterator  const_iterator;
  
-  inline const_iterator cbegin() const { return _list.cbegin(); }
-  inline const_iterator cend() const { return _list.cend(); }
+  inline const_iterator cbegin() const {
+    return _list.cbegin();
+  }
+  inline const_iterator cend() const {
+    return _list.cend();
+  }
  
 private:
   std::vector< T const * > _list;
@@ -29,13 +35,17 @@ struct Statement {
 };
  
 struct YetAnotherStatement: Statement {
-  inline void Analyze() const final { std::cout << t << std::endl; }
+  inline void Analyze() const final {
+    std::cout << t << std::endl;
+  }
   YetAnotherStatement( int i ): t{ ( double ) i * ( 10.6 / 0.7 ) } {}
   double t;
 };
  
 struct OtherStatement: Statement {
-  inline void Analyze() const final { std::cout << t << std::endl; }
+  inline void Analyze() const final {
+    std::cout << t << std::endl;
+  }
   OtherStatement( int i ): t{ i } {}
   int t;
 };
@@ -49,10 +59,13 @@ struct CompoundStatement: Statement, List<Statement> {
   CompoundStatement& operator=(CompoundStatement&&) = delete;
   
   ~CompoundStatement(){
-    for( auto b = cbegin(), d = cend(); b != d; ++b ) delete const_cast<Statement *>( *b );
+    for ( auto b = cbegin(), d = cend(); b != d; ++b ) {
+      delete const_cast<Statement *>( *b );
+    }
   }
+  
   void Analyze() const final {
-    for( auto b = this->cbegin(); b != this->cend(); ++b ){
+    for ( auto b = this->cbegin(); b != this->cend(); ++b ) {
       (*b)->Analyze();
     }
   }
@@ -60,15 +73,20 @@ struct CompoundStatement: Statement, List<Statement> {
  
 struct Declaration {
   Declaration( Statement const * const s ): s_( s ){}
-  inline void Analyze(){ s_->Analyze(); }
+  inline void Analyze() {
+    s_->Analyze();
+  }
   Statement const * const s_;
 };
  
 int main() {
   auto s = std::make_unique<CompoundStatement>();
-  for( int i = 1; i <= 10; ++i ){
-    if( i % 2 == 0 ) s->Append( new OtherStatement( i ) );
-    else s->Append( new YetAnotherStatement( i ) );
+  for ( int i = 1; i <= 10; ++i ) {
+    if( i % 2 == 0 ) {
+      s->Append( new OtherStatement( i ) );
+    } else {
+      s->Append( new YetAnotherStatement( i ) );
+    }
   }
   Statement const * const p_s = s.get();
   Declaration d( p_s );
