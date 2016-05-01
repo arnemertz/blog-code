983335d02b33be839ba8fcee682a03b49205cac6
Arne Mertz <void@arne-mertz.de>
2016-05-01 14:23:02 +0200

    Example visitor: expression printer

--- 2016-04-visitor-pattern/main.cpp
+++ 2016-04-visitor-pattern/main.cpp
@@ -1,5 +1,34 @@
+#include <iostream>
 #include <memory>
 #include "expressions.h"
+#include "expressionvisitor.h"
+
+class ExpressionPrinter : public ExpressionVisitor {
+  std::ostream& os;
+  
+  void visitBinaryExpression(BinaryExpression& binExpr, std::string const& infix) {
+    binExpr.left().accept(*this);
+    os << infix;
+    binExpr.right().accept(*this);
+  }
+  
+public:
+  ExpressionPrinter(std::ostream& ostream) : os(ostream) {}
+  void print(Expression& expr) {
+    expr.accept(*this);
+    os << '\n';
+  }
+
+  void visitAdd(AddExpression& addExpr) override {
+    visitBinaryExpression(addExpr, " + ");  
+  }
+  void visitMultiply(MultiplyExpression& mulExpr) override {
+    visitBinaryExpression(mulExpr, " * ");  
+  }
+  void visitNumber(NumberExpression& numExpr) override {
+    os << numExpr.getNumber();
+  }
+};
 
 int main() {
   auto expression = std::make_unique<AddExpression>(
@@ -9,4 +38,7 @@ int main() {
       std::make_unique<NumberExpression>(6)
     )
   );
+  
+  ExpressionPrinter printer(std::cout);
+  printer.print(*expression);
 }
